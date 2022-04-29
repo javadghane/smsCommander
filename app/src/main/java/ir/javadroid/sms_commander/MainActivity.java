172c25a0063a572e1e9efe81b15f0a668a6ea4d6
Javad Ghane <hi@javadroid.ir>
2022-04-30 02:25:44 +0430

    fix event bus

--- app/src/main/java/ir/javadroid/sms_commander/MainActivity.java
+++ app/src/main/java/ir/javadroid/sms_commander/MainActivity.java
@@ -49,6 +49,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(sent);
